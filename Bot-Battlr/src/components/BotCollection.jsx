807e5b6b0df6b7760b825641dae61ec0226889c4
fredrick-gachihi <fredrickgachihi24@gmail.com>
2024-05-05 12:27:23 +0300

    bot collection

---
+++ Bot-Battlr/src/components/BotCollection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function BotCollection() {
+  return (
+    <div className="grid grid-cols-4 gap-4">
+      <div className='w-full'>
+        <img src='https://robohash.org/nostrumrepellendustenetur.png?size=300x300&set=set1'
+        alt=''
+        className='w-full-[250px] object-cover'
+        />
+        <div>
+           <h5 className='font-bold text-xl'>wHz-93</h5>
+           <h5 className='font-semibold'>1010010101001101100011000111101</h5>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BotCollection
